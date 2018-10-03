049f0e7eedf303fe766c832c09cb0829739d6f08
EC2 Default User <ec2-user@ip-172-31-7-93.ec2.internal>
2018-10-03 16:06:30 +0000

    Most of the frontend is made - still need to get color scheme and bullte point items

---
+++ backend.js
@@ -0,0 +1,45 @@
+          $(document).ready(function() {
+            $(".searchbox").on("keypress", function(e) {
+              if (e.keyCode == 13) {
+                search();
+              }
+            })
+          });
+          function search(str) {
+            $.ajax({
+              url: "https://en.wikipedia.org/w/api.php",
+              jsonp: "callback",
+              dataType: 'jsonp',
+              data: {
+                action: "query",
+                list: "search",
+                srsearch: $(".searchbox").val(),
+                srinfo: "suggestion",
+                srlimit: "10",
+                format: "json"
+              },
+              xhrFields: {
+                withCredentials: true
+              },
+              success: displayResults
+            });
+          
+          }
+          function displayResults(data) {
+              $('#results').empty();
+              
+              $.each(data.query.search, function(index, obj) {
+                var outstring = "";
+                outstring += "<a href='https://en.wikipedia.org/wiki/" + obj.title.replace(" ", "_") + "' target=&quot;_blank&quot;>";
+                outstring += "<div class='result'>";
+                outstring += "<p class='title'>";
+                outstring += obj.title;
+                outstring += "</p>";
+                outstring += "<p class='snippet'>"
+                outstring += obj.snippet;
+                outstring += "</p>";
+                outstring += "</div>";
+                outstring += "</a>";
+                $("#results").append(outstring);
+              }); 
+          }
